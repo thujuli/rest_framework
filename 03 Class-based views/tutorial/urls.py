3da664b83a87de18cc7378fba0c3b617c39c08c7
Thu Juli <thujuli037@gmail.com>
2022-05-29 20:23:34 +0800

    class based view

--- 03 Class-based views/tutorial/urls.py
+++ 03 Class-based views/tutorial/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
